0db45a1709f7553fc38ac043ed11e5c4bd0a13da
your full name here <gserwaa998@gmail.com>
2024-11-03 04:48:13 +0000

    docs:format ClientException file

--- SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/Exception/ClientException.java
+++ SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/Exception/ClientException.java
@@ -1,11 +1,12 @@
 package com.georgia.SimpleClientManagement.Exception;
+
 import org.springframework.http.HttpStatus;
 
 public class ClientException {
     private final String message;
     private final HttpStatus httpStatus;
 
-    public ClientException(String message,HttpStatus httpStatus) {
+    public ClientException(String message, HttpStatus httpStatus) {
         this.message = message;
         this.httpStatus = httpStatus;
     }
