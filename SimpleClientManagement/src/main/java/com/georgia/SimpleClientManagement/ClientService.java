fc32a5c629c8c13b1b5da5287281123571e1e296
your full name here <gserwaa998@gmail.com>
2024-11-03 05:25:50 +0000

    DOCS:Format ClientService file

--- SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/ClientService.java
+++ SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/ClientService.java
@@ -7,29 +7,29 @@ import com.georgia.SimpleClientManagement.Exception.ClientNotFoundException;
 
 @Service
 public class ClientService {
-   @Autowired
-   private ClientRepository clientRepository;
+    @Autowired
+    private ClientRepository clientRepository;
 
-   public ClientService (ClientRepository clientRepository) {
-    this.clientRepository = clientRepository;
-   }
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
-     
     public List<Client> getAllClients() {
         return clientRepository.findAll();
 
-}
+    }
 
-public Client getClient(Long clientId) {
-    if(clientRepository.findById(clientId).isEmpty())
-        throw new ClientNotFoundException("Requested Client Profile does not exist");
-    return clientRepository.findById(clientId).get();
+    public Client getClient(Long clientId) {
+        if (clientRepository.findById(clientId).isEmpty())
+            throw new ClientNotFoundException("Requested Client Profile does not exist");
+        return clientRepository.findById(clientId).get();
     }
 
     public String createClient(Client client) {
         clientRepository.save(client);
         return "Success";
     }
+
     public String updateClient(Long id, Client updatedClient) {
         // Check if the client exists
         Client existingClient = clientRepository.findById(id)
@@ -44,14 +44,11 @@ public Client getClient(Long clientId) {
         // Save the updated client
         clientRepository.save(existingClient);
         return "Client profile updated successfully";
-   
 
     }
 
     // Delete an Client by its ID
     public void deleteClient(Long id) {
-    clientRepository.deleteById(id);
+        clientRepository.deleteById(id);
     }
 }
-
-   
