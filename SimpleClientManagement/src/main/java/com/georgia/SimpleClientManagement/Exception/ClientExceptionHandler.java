0dae1c3ab6866061a4d4d096ac77b8fb2d9a89b7
your full name here <gserwaa998@gmail.com>
2024-11-03 05:13:43 +0000

    DOCS:Format ClientExceptionHandler file

--- SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/Exception/ClientExceptionHandler.java
+++ SimpleClientManagement/src/main/java/com/georgia/SimpleClientManagement/Exception/ClientExceptionHandler.java
@@ -1,19 +1,15 @@
 package com.georgia.SimpleClientManagement.Exception;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 public class ClientExceptionHandler {
 
-    @ExceptionHandler(value = {ClientNotFoundException.class})
-    public ResponseEntity<Object> handleClientNotFoundException
-            (ClientNotFoundException clientNotFoundException)
-    {
+    @ExceptionHandler(value = { ClientNotFoundException.class })
+    public ResponseEntity<Object> handleClientNotFoundException(ClientNotFoundException clientNotFoundException) {
         ClientException clientException = new ClientException(
                 clientNotFoundException.getMessage(),
                 HttpStatus.NOT_FOUND);
@@ -21,5 +17,3 @@ public class ClientExceptionHandler {
         return new ResponseEntity<>(clientException, HttpStatus.NOT_FOUND);
     }
 }
-
-
