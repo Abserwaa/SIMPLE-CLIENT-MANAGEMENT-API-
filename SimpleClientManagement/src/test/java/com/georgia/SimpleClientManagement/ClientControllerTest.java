a7127d8e3fd3777de7d3f299be1f7e45c05659a8
your full name here <gserwaa998@gmail.com>
2024-12-01 15:06:35 +0000

    DOCS:Update @Mockbean to @Mock in Controller Test file.

--- SimpleClientManagement/src/test/java/com/georgia/SimpleClientManagement/ClientControllerTest.java
+++ SimpleClientManagement/src/test/java/com/georgia/SimpleClientManagement/ClientControllerTest.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,7 +28,7 @@ public class ClientControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private ClientService clientService;
 
     private Client testClient;
